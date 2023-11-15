8f97f3a81c57f6ee59d0344975002d6c2805763b
Gioele Pannetto <gioele@pannetto.com>
2023-11-15 22:06:22 +0100

    Exception for wakatime pro

--- lib/widgets/exception.dart
+++ lib/widgets/exception.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class ExceptionButton extends StatelessWidget {
   final Object error;
@@ -36,16 +38,35 @@ class ExceptionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('An error occured'),
-        const SizedBox(height: 5),
-        OutlinedButton(
-          onPressed: () => _openDialog(context),
-          child: const Text('Details'),
-        ),
-      ],
+      children: error.runtimeType == DioException &&
+              (error as DioException).response?.statusCode == 402
+          ? [
+              const Icon(
+                LucideIcons.euro,
+                size: 48,
+              ),
+              const SizedBox(height: 12),
+              Text(
+                'Pro account required',
+                style: theme.textTheme.bodyLarge,
+              ),
+              Text(
+                'This action requires a WakaTime pro account',
+                style: theme.textTheme.bodySmall,
+              ),
+            ]
+          : [
+              const Text('An error occured'),
+              const SizedBox(height: 5),
+              OutlinedButton(
+                onPressed: () => _openDialog(context),
+                child: const Text('Details'),
+              ),
+            ],
     );
   }
 }
