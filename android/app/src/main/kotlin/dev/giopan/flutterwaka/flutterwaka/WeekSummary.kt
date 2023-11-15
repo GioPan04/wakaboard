4aaaee34099119ae98b6db45b003374aff087ea9
Gioele Pannetto <gioele@pannetto.com>
2023-11-16 00:49:00 +0100

    Basic home screen widget

---
+++ android/app/src/main/kotlin/dev/giopan/flutterwaka/flutterwaka/WeekSummary.kt
@@ -0,0 +1,51 @@
+package dev.giopan.flutterwaka.flutterwaka
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin
+import java.io.File
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class WeekSummary : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.week_summary).apply {
+                // Get chart image and put it in the widget, if it exists
+                val imagePath = widgetData.getString("weekSummaryChart", null)
+                if(imagePath == null) return
+                val imageFile = File(imagePath)
+                val imageExists = imageFile.exists()
+                if (imageExists) {
+                    val myBitmap: Bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+                    setImageViewBitmap(R.id.widget_image, myBitmap)
+                } else {
+                    println("image not found!, looked @: $imagePath")
+                }
+                // End new code
+            }
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
